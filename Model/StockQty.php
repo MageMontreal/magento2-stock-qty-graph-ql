79c7de90fcad998c128912dfccb25b1018e36517
Carlos Gartner <contato@carlsgartner.com.br>
2021-09-22 10:00:42 -0300

    Updated Exception

--- Model/StockQty.php
+++ Model/StockQty.php
@@ -7,6 +7,7 @@
 namespace MageMontreal\StockQtyGraphQl\Model;
 
 use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
 use Magento\Framework\GraphQl\Query\Resolver\ContextInterface;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
@@ -40,11 +41,12 @@ class StockQty implements ResolverInterface
      * @param array|null $value
      * @param array|null $args
      * @return int
+     * @throws GraphQlInputException
      */
     public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null): int
     {
         if (!isset($value['model'])) {
-            throw new LocalizedException(__('"model" value should be specified'));
+            throw new GraphQlInputException(__('"model" value should be specified'));
         }
         return $this->getStockBySku($value['sku']);
     }
